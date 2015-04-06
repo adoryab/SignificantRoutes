df463f9375fb3c859b01d74ef67026a5014c6475
Rajat Mehndiratta <rajatmehndiratta@cmu.edu>
2015-04-06 13:07:19 -0400

    Readded update lines, currently testing it and improving speed

--- sqlite_update.py
+++ sqlite_update.py
@@ -3,8 +3,15 @@
 # calls Google's Reverse Geocoding API to add location data
 # to an SQLite database containing latitude/longitude values
 
+# @TODO:
+# 1. speed up update function
+# 2. improve output sanitation in parseLocationData
+# 3. make code less specific to this particular database
+# 4. set this up to work with command line
+# 5. verify that path to database is valid (sqlite3 doesn't check)
+
 API_Key = "AIzaSyCN84vI7drlWNjVwPM-pNLZnV5HW0OAEVc"
-# unnecessary for these calls, just used for monitoring
+# unnecessary for these particular calls, just used for monitoring
 # 5 calls/second and 25,000 calls/day on this key
 
 URL = "https://maps.googleapis.com/maps/api/geocode/json"
@@ -29,26 +36,38 @@ def parseLocationData(jsonObject):
 				name = element['short_name']
 				locType = element['types'][0]
 				# @TODO: rewrite next few lines
-				if "sublocality" in locType: locType = "sublocality"
-				elif (locType == "administrative_area_level_2"):
-					locType = "county"
-				elif (locType == "administrative_area_level_1"):
-					locType = "city"
+                if ("sublocality" in locType): locType = "sublocality"
+                elif (locType == "administrative_area_level_2"): locType = "county"
+                elif (locType == "administrative_area_level_1"): locType = "state"
+                elif (locType == "route"): locType = "street"
+                elif (locType == "political"): locType = "city"
 				# @TODO: rewrite previous few lines
-				result[locType] = name
+                result[locType] = name
 	return result
 
-def update(database="locations.db"):
+def update(database="locations.db", table="locations", 
+           columns=["street", "locality", "neighborhood", "city", "county", "state",
+                    "country", "postal_code"], id_col="_id"):
     con = sqlite3.connect(database)
     cursor = con.cursor()
-    cursor.execute("select * from locations")
+    for col in columns:
+        cursor.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' TEXT"\
+            .format(tn=table, cn=col))
+    cursor.execute("SELECT * FROM {tn}".format(tn=table))
     fetch = cursor.fetchone()
     while fetch is not None:
-    	if fetch[0]%10 == 0: print fetch[0]
-    	latitude = fetch[3]
-    	longitude = fetch[4]
+        id_num = fetch[0]
+        print "current ID", id_num
+    	latitude = fetch[3] # @TODO: modify so that these lines aren't specific
+    	longitude = fetch[4] # to just this database
     	jsonObject = getLocationData(latitude, longitude)
     	result = parseLocationData(jsonObject)
     	fetch = cursor.fetchone()
+        for item in result:
+            if item in columns:
+                c.execute("UPDATE {tn} SET {cn}=('{value}') WHERE {idc}=({ID})".\
+                          format(tn=table, cn=item, idc=id_col, ID=id_num))
+    con.commit()
+    con.close()
 
 update()
